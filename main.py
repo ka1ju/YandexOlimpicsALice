4bd515169be3073e000758ba0abf90d42d528713
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-17 20:50:21 +0700

    попробовал создать костяк для навыка

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from flask import *
+import logging
 import json
 
-f = open('funcs.json', 'r', encoding='utf-8')
-x = json.load(f)
-for i in x:
-    print(i + ':')
-    print('\tПояснение:', x[i][0])
-    print('\tОбращение к Алисе:', x[i][1][0])
-    print('\tПояснения к переменым:', *x[i][1][1], '\n')
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route('/', methods=["POST"])
+def hello():
+    logging.info(request.json)
+
+    response = \
+        {
+            "version": request.json["version"],
+            "session": request.json["session"],
+            "response":
+                {
+                    "end_session": False
+                }
+        }
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Salam."
+
+    return json.dumps(response)
