51506e8b0fde42034b0e751ccd77e8ea21ea5dcb
Sleepy-Fenrir <103877166+Sleepy-Fenrir@users.noreply.github.com>
2022-04-23 23:34:03 +0700

    Траты (почти всё)

--- wasting.py
+++ wasting.py
@@ -3,8 +3,10 @@ from db_working import to_db, from_db, change_db, remove_from_db
 
 def wasting(s, user_name):
     list = [('развлечения', []), ('продукты', []), ('налоги', []), ('магазины', []), ('другое', [])]
+    list2 = [] # здесь будут вообще все категории, которые раскиданы по обобщающим категориям выше
     user_id1 = [i.id for i in from_db("users", "Users", {"Username": user_name})]
     user_id = user_id1[0]
+    n = ''
     N = ''
     x = 0
     t = ''
@@ -19,9 +21,13 @@ def wasting(s, user_name):
         if i[0:-3] in names:
             N = i
             break
-
-
-
+    for i in a:
+        if i in list2:
+            n = i
+            break
+    for i in range(len(list)):
+        if n in list[i][1]:
+            N = list[i][0]
     id1 = [i.id for i in from_db("accounts", "Accounts", {"account": N, "user_id": user_id})]
     id = id1[0]
     summ1 = [j.bank for j in from_db("accounts", "Accounts", {"account": N, "user_id": user_id})]
@@ -34,3 +40,4 @@ def wasting(s, user_name):
     change_db("waste", "Waste", {"count": newcount}, {"account_id": id, "category": t})
 # N - название счёта, x - сумма, t - категория
 # частично сделал распознавание
+# осталось только норм категории раскидать и вроде всё
