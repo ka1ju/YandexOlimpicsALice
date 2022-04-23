d2fa76b1bdd6f145d482d0ff8e7b4449fd7ee5b4
Федор <89134697362f@gmail.com>
2022-04-23 16:53:00 +0700

    функция пополнения3

--- Popolnyt.py
+++ Popolnyt.py
@@ -8,10 +8,16 @@ def Popolnyt(st, un):
     #проверка юзера
     for i in range(len(b)):
         a.append(' '.join(b[i]).split(' '))
+    x = [el.accounts for el in bd]
+    y = 0
     for j in range(len(a)):
         for i in range(len(a[j])):
-            if a[j][i] in [el.accounts for el in bd]:
+            if a[j][i] in x:
+                y = 1
                 v = from_db('accounts', 'Accounts', {'accounts': a[j][i]})
                 change_db('accounts', 'Accounts', {'accounts': a[j][i], 'bank': int(v[0].bank + int(a[j][i + 2]))})
-    return 'успешно'
+    if y == 1:
+        return 'Успешно'
+    else:
+        return 'У вас нет кошелька с таким именем'
     pass
