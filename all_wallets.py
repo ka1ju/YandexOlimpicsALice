5fff93eb63c0847ad09364fd31dbb5d2f0125492
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-24 21:19:14 +0700

    сделал почище код

--- all_wallets.py
+++ all_wallets.py
@@ -5,6 +5,6 @@ import pymorphy2
 def return_wallets(user_id):
     u_id = [i.id for i in from_db("users", 'Users', {"username": user_id})][0]
     wallets = ['"' + i.accounts + '" - ' + str(i.bank) + ' ' +
-               pymorphy2.MorphAnalyzer().parse(i.currency)[0].make_agree_with_number(i.bank).inflect({'gent'}).word + '.'
-               for i in from_db("accounts", "Accounts", {"user_id": u_id})]
+               pymorphy2.MorphAnalyzer().parse(i.currency)[0].make_agree_with_number(i.bank).inflect({'gent'}).word
+               + '.' for i in from_db("accounts", "Accounts", {"user_id": u_id})]
     return '\n'.join(wallets)
