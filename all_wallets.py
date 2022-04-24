6ae156c9276b1cfa56e6e610da5d98b82505ebc0
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-24 15:01:38 +0700

    Обновил свой файл для вывода функций. Для теста артёмкиных функций для бд

--- all_wallets.py
+++ all_wallets.py
@@ -1,6 +1,6 @@
 from db_working import *
 
 
-def return_wallets():
-    wallets = from_db("accounts", "accounts", )
-    return '\n'.join(wallets)
+def return_wallets(user_id):
+    wallets = [i for i in from_db("accounts", "Accounts", {"user_id": user_id})]
+    return wallets
