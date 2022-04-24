884004cb1f74b5cf13760addfde5b5c7bd58d339
Zhaba <t.aleksandrovich@ngs.ru>
2022-04-24 12:57:54 +0700

    создал if для вывода всех кошельков

--- oleg.py
+++ oleg.py
@@ -1,39 +1,41 @@
 from db_working import *
 
 
-def authorization(usr_name):
-    a = from_db('users', 'Users', {'username' :usr_name})
+def authorization(user_name):
+    a = from_db('users', 'Users', {'username': user_name})
     if a:
         return 'Приветствую, приятного пользования'
     else:
-        to_db('users', 'Users', ['username', 'password'], [usr_name, '1'])
-        return 'Привет, меня зовут Олег. Я создан для того чтобы вести учёт ваших трат. Вот краткий список моих функций:\
-                Добавление кошелька\
-                Удаление кошелька\
-                Зачисление денег на счёт\
-                Снятие или трата денег со счёта\
-                Вывод статистики\
+        to_db('users', 'Users', ('username', 'password'), (user_name, '1'))
+        print(user_name)
+        return 'Привет, меня зовут Олег. Я создан для того чтобы вести учёт ваших трат. ' \
+               'Вот краткий список моих функций:\n\
+                Добавление кошелька\n\
+                Удаление кошелька\n\
+                Зачисление денег на счёт\n\
+                Снятие или трата денег со счёта\n\
+                Вывод статистики\n\
                 Вывод информации о счёте'
 
 
-def statistic(str, usr_name):
+def statistic(string, usr_name):
     a = from_db('users', 'Users', {'username': usr_name})[0]
-    id = a.id
-    yuy = from_db('accounts', 'Accounts', {'user_id' :id})
+    account_element_id = a.id
+    yuy = from_db('accounts', 'Accounts', {'user_id': account_element_id})
     names = [i.accounts for i in yuy]
     ac = ''
     for i in names:
-        if i in str.split():
+        if i in string.split():
             ac = i
     if ac != '':
-        yuy = from_db('accounts', 'Accounts', {'user_id': id, 'accounts' :ac})[0]
-        d = {'развлечения': 0, 'продукты':0, 'налоги':0, 'магазины':0, 'другое':0}
-        all = 0
-        ok = from_db('waste', 'Waste', {'account_id' :yuy.id})
+        yuy = from_db('accounts', 'Accounts', {'user_id': account_element_id, 'accounts': ac})[0]
+        d = {'развлечения': 0, 'продукты': 0, 'налоги': 0, 'магазины': 0, 'другое': 0}
+        spend_all = 0
+        ok = from_db('waste', 'Waste', {'account_id': yuy.id})
         for i in ok:
             d[i.category] += i.count
-            all += i.count
-        return f'В общем было потрачено: {all}\nНа развлечения: {d["развлечения"]}\n' \
+            spend_all += i.count
+        return f'В общем было потрачено: {spend_all}\nНа развлечения: {d["развлечения"]}\n' \
                f'На продукты: {d["продукты"]}\n' \
                f'На оплату налогов: {d["налоги"]}\n' \
                f'В магазинах: {d["магазины"]}' \
