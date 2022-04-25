14e16993429c9048a7d538ff87e74eb5af94546c
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-25 21:57:50 +0700

    убрал лишний кусок в 1 строке

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
 
 def handle_dialog(req, res):
     user_id = req['session']['user_id']
-    user_message: object = req['request']['original_utterance'].lower()
+    user_message = req['request']['original_utterance'].lower()
 
     if req['session']['new']:
         res['response']['text'] = authorization(user_id)
