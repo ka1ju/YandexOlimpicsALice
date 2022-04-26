3df79b2e3ad24e74a502e9884c1819048f58c6cd
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-26 20:05:05 +0700

    Заменил txt на json и теперь не удаляю файл каждый раз как даун

--- all_wallets.py
+++ all_wallets.py
@@ -1,8 +1,8 @@
 from db_working import *
 import pymorphy2
 import random
-import os
 import logging
+import json
 
 
 def return_wallets(user_id):
@@ -11,18 +11,18 @@ def return_wallets(user_id):
                pymorphy2.MorphAnalyzer().parse(i.currency)[0].make_agree_with_number(i.bank).inflect({'gent'}).word
                + '.' for i in from_db("accounts", "Accounts", {"user_id": u_id})]
     if len(wallets) > 0:
+
         return "Вот ваши кошельки:\n" + '\n'.join(wallets)
     else:
         answers = ["Пока что кошельков нет", "Извините, но я не нашел у вас ни одного кошелька.",
                    "Вы пока что не создали ни один счёт"]
-        message = open("prev_alice_message.txt", "r", encoding="utf-8")
-        m = message.read()
-        if m in answers:
-            answers.remove(m)
+        message = open("prev_alice_message.json", "r", encoding="utf-8")
+        m = json.load(message)
+        if m["previous_message"] in answers:
+            answers.remove(m["previous_message"])
         message.close()
         index = random.randint(0, len(answers) - 1)
         answer = answers[index]
-        os.remove("prev_alice_message.txt")
-        with open("prev_alice_message.txt", "w", encoding="utf-8") as out:
-            out.write(answer)
+        with open("prev_alice_message.json", "w", encoding="utf-8") as out:
+            json.dump(m, out)
         return answer
