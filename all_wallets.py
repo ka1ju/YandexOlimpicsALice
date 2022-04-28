ca618d66b2d4c50f835babebb33954320bfc3b4f
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-04-28 16:52:49 +0700

    теперь нет тенгов))

--- all_wallets.py
+++ all_wallets.py
@@ -8,7 +8,8 @@ def return_wallets(user_id):
     u_id = [i.id for i in from_db("users", 'Users', {"username": user_id})][0]
     wallets = ['"' + i.accounts + '" - ' + str(i.bank) + ' ' +
                pymorphy2.MorphAnalyzer().parse(i.currency)[0].make_agree_with_number(i.bank).inflect({'gent'}).word
-               + '.' for i in from_db("accounts", "Accounts", {"user_id": u_id})]
+               + '.'if i.currency != "тенге" else '"' + i.accounts + '" - ' + str(i.bank) + ' тенге'
+               for i in from_db("accounts", "Accounts", {"user_id": u_id})]
     if len(wallets) > 0:
         return "Вот ваши кошельки:\n" + '\n'.join(wallets)
     else:
