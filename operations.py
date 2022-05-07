b8f03bee90b6f402e1a72546b9eeb0bdc50f4c86
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-05-07 15:31:49 +0700

    доделал вывод операций

--- operations.py
+++ operations.py
@@ -5,33 +5,37 @@ import pymorphy2
 
 
 def operations(userName, userMessage):
+    userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
+    walletId = ""
+    for i in from_db("accounts", "Accounts", {"user_id": userId}):
+        if i.accounts in userMessage:
+            walletId = i.id
+            break
+    if type(walletId) == str:
+        return "Извините, такого кошелька не существует"
     if "месяц" in userMessage:
         date = datetime.date.today() - timedelta(days=30)
-        result = ["Ваши операции за последний месяц:"]
+        operationsList = ["Ваши операции за последний месяц:"]
     elif "год" in userMessage:
         date = datetime.date.today() - timedelta(days=365)
-        result = ["Ваши операции за последний год:"]
+        operationsList = ["Ваши операции за последний год:"]
     else:
         date = datetime.date.today() - timedelta(weeks=1)
-        result = ["Ваши операции за последнюю неделю:"]
-    userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
-    currencies = {}
-    walletsId = [i.id for i in from_db("accounts", "Accounts", {"user_id": userId})]
+        operationsList = ["Ваши операции за последнюю неделю:"]
+    currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
     for i in from_db("waste", "Waste"):
         d = i.date.split(".")
         if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
-            walletId = i.account_id
-            if walletId in walletsId:
-                if i.category != "":
-                    currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
-                    if currency in currencies:
-                        currencies[currency] += int(i.count)
-                    else:
-                        currencies[currency] = int(i.count)
-    for i in currencies:
-        if i == "тенге":
-            result.append(str(currencies[i]) + " " + i)
-        else:
-            cur = pymorphy2.MorphAnalyzer().parse(i)[0].make_agree_with_number(currencies[i]).inflect({'gent'}).word
-            result.append(str(currencies[i]) + " " + cur)
-    return "\n".join(result)
+            if i.account_id == walletId:
+                if currency != "тенге":
+                    cur = pymorphy2.MorphAnalyzer().parse(currency)[0].make_agree_with_number(i.count).inflect({'gent'}).word
+                else:
+                    cur = currency
+                if i.category == "":
+                    operationsList.append(f"Получено {i.count} {cur}")
+                else:
+                    operationsList.append(f"Потрачено {i.count} {cur} на {i.category}")
+    return "\n".join(operationsList)
+
+
+print(operations("AQAAAAAjstOuAAfaP9kRfOsQJ00rlrOmg7BlREQ", "Выведи все мои операции по счёту тест за месяц"))
