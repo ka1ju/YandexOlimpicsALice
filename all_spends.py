4faf108bea179104446ae85460fa04e63da9d81c
Zherebtsov Yaroslav <n0name_off@mail.ru>
2022-05-08 18:11:47 +0700

    убрал баги

--- all_spends.py
+++ all_spends.py
@@ -14,27 +14,32 @@ def all_spends(userName, userMessage):
     else:
         date = datetime.date.today() - timedelta(days=30)
         result = ["Ваши траты за последний месяц:"]
-    userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
-    currencies = {}
-    walletsId = [i.id for i in from_db("accounts", "Accounts", {"user_id": userId})]
-    for i in from_db("waste", "Waste"):
-        d = i.date.split(".")
-        if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
-            walletId = i.account_id
-            if walletId in walletsId:
-                if i.category != "":
-                    currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
-                    if currency in currencies:
-                        currencies[currency] += int(i.count)
-                    else:
-                        currencies[currency] = int(i.count)
-    for i in currencies:
-        if i == "тенге":
-            result.append(str(currencies[i]) + " " + i)
-        else:
-            cur = pymorphy2.MorphAnalyzer().parse(i)[0].make_agree_with_number(currencies[i]).inflect({'gent'}).word
-            result.append(str(currencies[i]) + " " + cur)
-    return "\n".join(result)
+    try:
+        userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
+        currencies = {}
+        walletsId = [i.id for i in from_db("accounts", "Accounts", {"user_id": userId})]
+        for i in from_db("waste", "Waste"):
+            d = i.date.split(".")
+            if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
+                walletId = i.account_id
+                if walletId in walletsId:
+                    if i.category != "пополнение":
+                        currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
+                        if currency in currencies:
+                            currencies[currency] += int(i.count)
+                        else:
+                            currencies[currency] = int(i.count)
+        for i in currencies:
+            if i == "тенге":
+                result.append(str(currencies[i]) + " " + i)
+            else:
+                cur = pymorphy2.MorphAnalyzer().parse(i)[0].make_agree_with_number(currencies[i]).inflect({'gent'}).word
+                result.append(str(currencies[i]) + " " + cur)
+        if len(result) == 1:
+            return "За выбранный период вы не потратили ничего."
+        return "\n".join(result)
+    except IndexError:
+        return "No such a User"
 
 
 # print(all_spends("AQAAAAAjstOuAAfaP9kRfOsQJ00rlrOmg7BlREQ", ""))
--- operations.py
+++ operations.py
@@ -5,67 +5,74 @@ import pymorphy2
 
 
 def operations(userName, userMessage, state):
-    if state == {}:
-        if "месяц" in userMessage:
-            date = datetime.date.today() - timedelta(days=30)
-            operationsList = ["Ваши операции за последний месяц:"]
-        elif "год" in userMessage:
-            date = datetime.date.today() - timedelta(days=365)
-            operationsList = ["Ваши операции за последний год:"]
-        else:
-            date = datetime.date.today() - timedelta(weeks=1)
-            operationsList = ["Ваши операции за последнюю неделю:"]
-        userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
-        walletId = ""
-        for i in from_db("accounts", "Accounts", {"user_id": userId}):
-            if i.accounts in userMessage:
-                walletId = i.id
-                break
-        if type(walletId) == str:
-            return "По какому кошельку вы хотите вывести операции", \
-                   {"date": {"day": date.day, "month": date.month, "year": date.year}, "operationsList": operationsList}
-        currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
-        for i in from_db("waste", "Waste"):
-            d = i.date.split(".")
-            if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
-                if i.account_id == walletId:
-                    if currency != "тенге":
-                        cur = pymorphy2.MorphAnalyzer().parse(currency)[0].make_agree_with_number(i.count).inflect(
-                            {'gent'}).word
-                    else:
-                        cur = currency
-                    if i.category == "пополнение":
-                        operationsList.append(f"Получено {i.count} {cur}")
-                    else:
-                        operationsList.append(f"Потрачено {i.count} {cur} на {i.category}")
-        return "\n".join(operationsList), {}
-    elif "date" in state:
-        date = datetime.date(year=state['date']['year'], month=state['date']['month'], day=state["date"]['day'])
-        operationsList = state["operationsList"]
-        userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
-        walletId = ""
-        for i in from_db("accounts", "Accounts", {"user_id": userId}):
-            if i.accounts in userMessage:
-                walletId = i.id
-                break
-        if type(walletId) == str:
-            return "Извините, такого кошелька не существует, отменяю операцию", {}
-        currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
-        for i in from_db("waste", "Waste"):
-            d = i.date.split(".")
-            if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
-                if i.account_id == walletId:
-                    if currency != "тенге":
-                        cur = pymorphy2.MorphAnalyzer().parse(currency)[0].make_agree_with_number(i.count).inflect(
-                            {'gent'}).word
-                    else:
-                        cur = currency
-                    if i.category == "пополнение":
-                        operationsList.append(f"Получено {i.count} {cur}")
-                    else:
-                        operationsList.append(f"Потрачено {i.count} {cur} на {i.category}")
-        return "\n".join(operationsList), {}
-    return "Что-то пошло нахуй, например твоя мать", {}
+    try:
+        if state == {}:
+            if "месяц" in userMessage:
+                date = datetime.date.today() - timedelta(days=30)
+                operationsList = ["Ваши операции за последний месяц:"]
+            elif "год" in userMessage:
+                date = datetime.date.today() - timedelta(days=365)
+                operationsList = ["Ваши операции за последний год:"]
+            else:
+                date = datetime.date.today() - timedelta(weeks=1)
+                operationsList = ["Ваши операции за последнюю неделю:"]
+            userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
+            walletId = ""
+            for i in from_db("accounts", "Accounts", {"user_id": userId}):
+                if i.accounts in userMessage:
+                    walletId = i.id
+                    break
+            if type(walletId) == str:
+                return "По какому кошельку вы хотите вывести операции", \
+                       {"date": {"day": date.day, "month": date.month, "year": date.year}, "operationsList": operationsList}
+            currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
+            for i in from_db("waste", "Waste"):
+                d = i.date.split(".")
+                if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
+                    if i.account_id == walletId:
+                        if currency != "тенге":
+                            cur = pymorphy2.MorphAnalyzer().parse(currency)[0].make_agree_with_number(i.count).inflect(
+                                {'gent'}).word
+                        else:
+                            cur = currency
+                        if i.category == "пополнение":
+                            operationsList.append(f"Получено {i.count} {cur}")
+                        else:
+                            operationsList.append(f"Потрачено {i.count} {cur} на {i.category}")
+            if len(operationsList) == 1:
+                return "За выбранный период по данному счёту не было ни одной операции.", {}
+            return "\n".join(operationsList), {}
+        elif "date" in state:
+            date = datetime.date(year=state['date']['year'], month=state['date']['month'], day=state["date"]['day'])
+            operationsList = state["operationsList"]
+            userId = [i.id for i in from_db("users", "Users", {"username": userName})][0]
+            walletId = ""
+            for i in from_db("accounts", "Accounts", {"user_id": userId}):
+                if i.accounts in userMessage:
+                    walletId = i.id
+                    break
+            if type(walletId) == str:
+                return "Извините, такого кошелька не существует, отменяю операцию", {}
+            currency = [i.currency for i in from_db("accounts", "Accounts", {"id": walletId})][0]
+            for i in from_db("waste", "Waste"):
+                d = i.date.split(".")
+                if datetime.date(year=int(d[2]), month=int(d[1]), day=int(d[0])) > date:
+                    if i.account_id == walletId:
+                        if currency != "тенге":
+                            cur = pymorphy2.MorphAnalyzer().parse(currency)[0].make_agree_with_number(i.count).inflect(
+                                {'gent'}).word
+                        else:
+                            cur = currency
+                        if i.category == "пополнение":
+                            operationsList.append(f"Получено {i.count} {cur}")
+                        else:
+                            operationsList.append(f"Потрачено {i.count} {cur} на {i.category}")
+            if len(operationsList) == 1:
+                return "За выбранный период по данному счёту не было ни одной операции.", {}
+            return "\n".join(operationsList), {}
+        return "Что-то пошло нахуй, например твоя мать", {}
+    except IndexError:
+        return "No such a user"
 
 
 # print(operations("AQAAAAAjstOuAAfaP9kRfOsQJ00rlrOmg7BlREQ", "негр", {'date': datetime.date(2021, 5, 8), 'operationsList': ['Ваши операции за последний год:']}))
